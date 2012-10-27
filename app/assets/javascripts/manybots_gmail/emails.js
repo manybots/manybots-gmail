5a9360f5d2da17c6777ab3d363975c444c75c13f
Niko Roberts <niko@tasboa.com>
2012-10-28 00:14:31 +0100

    pulled JS out of HTML and made deferred loading happen individually

--- app/assets/javascripts/manybots_gmail/emails.js
+++ app/assets/javascripts/manybots_gmail/emails.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  $('.email-body').each(function() {
+    $(this).load($(this).data("load_url"));
+  });
+});
--- app/assets/javascripts/manybots_gmail/gmail.js
+++ app/assets/javascripts/manybots_gmail/gmail.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  $('.select').each(function() {
+    var parent = $(this).parent()
+    $(this).load($(this).data('load_url'), function() {
+      $('.submit', parent).removeAttr('disabled');
+    });
+  });
+});
